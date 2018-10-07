df9400b0d25d86cf17368015bfab4a27ecfd6903
Roman Istomin <rji5040@psu.edu>
2018-10-07 17:59:05 -0400

    you get check minus. you did not add any of the auxillary functions and your main file does not work without them.

--- Codes_HW2_final.m
+++ Codes_HW2_final.m
@@ -1,4 +1,6 @@
-%%%%%%%%%% Question 1**************
+%% Question 1**************
+% there is no Demand function in this repository, it oes not work without
+% it. you need to add the functions you use to repository too. 
 function [DA,DB,D0] = demand(vA,vB,pA,pB)
 DA = exp(vA - pA)/(1 + exp(vA - pA)+ exp(vB - pB));
 DB = exp(vB - pB)/(1 + exp(vA - pA)+ exp(vB - pB));
@@ -7,7 +9,7 @@ end
 
 [DA DB D0] = demand(2,2,1,1);
 
-%%%%%%%%%% Question 2**************
+%% Question 2**************
 function [FOCs] = foc(vA,vB,p)
 DA = exp(vA - p1)/(1 + exp(vA - p1)+ exp(vB - p2));
 DB = exp(vB - p2)/(1 + exp(vA - p1)+ exp(vB - p2));
@@ -19,7 +21,7 @@ handle = @(p) foc(2,2,p);
 p_initial = [1;1];   %%%%%%%%Initial value
 p = broyden(handle,p_initial);
 
-%%%%%%%%%% Question 5**************
+%% %%%%%%%% Question 5**************
 clear
 vA = 2;
 vB = [0:.2:3]';
@@ -39,7 +41,7 @@ hold on
 plot(vB,pB,':bs')
 legend('pA','pB')
 
-%%%%%%%%%% Question 4**************
+%% %%%%%%%% Question 4**************
 function [FOC_A,FOC_B] = trial(vA,vB,p1,p2)
 DA = exp(vA - p1)/(1 + exp(vA - p1)+ exp(vB - p2));
 DB = exp(vB - p2)/(1 + exp(vA - p1)+ exp(vB - p2));
